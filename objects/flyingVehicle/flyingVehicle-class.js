86c915bdd0e6d8490fe1a1fdcf33cdcfc5dcf072
L-Stewart <stewartalorin@gmail.com>
2019-03-26 20:36:11 -0700

    working on vehicle tests

--- objects/flyingVehicle/flyingVehicle-class.js
+++ objects/flyingVehicle/flyingVehicle-class.js
@@ -1,1 +1,34 @@
 'use strict';
+
+class FlyingVehicle {
+  constructor(name, engines){
+    this.name = name;
+    this.engines = engines;
+  }
+  on() {
+    return 'All ' + this.engines + ' rotors are spinning up.';
+  }
+  flying() {
+    return 'We are airborne';
+  }
+}
+
+class Helicopter extends FlyingVehicle {
+  constructor(name, engines){
+    super(name, 2);
+  }
+  news(){
+    return 'This just in ...';
+  }
+}
+
+class Airplane extends FlyingVehicle {
+  constructor(name, engines){
+    super(name, 4);
+  }
+  errr(){
+    return 'Thank you for flying with us today.';
+  }
+}
+
+module.exports = {Helicopter, Airplane};
---
+++ objects/flyingVehicle/index.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const FlyingVehicle = require('./flyingVehicle-class');
+// const FlyingVehicle = require('./flyingVehicle-factory');
+
